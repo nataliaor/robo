a028801f058601457982d26090f6d2f0fdd185bb
nataliaor <talia.origo@gmail.com>
2020-02-15 22:05:21 +0000

    experimenting with returning colour detection success

--- colourdetect/col_detect.py
+++ colourdetect/col_detect.py
@@ -5,22 +5,25 @@ import numpy as np
  
  
  
-img = cv2.imread('../../colourtest.jpg')
+img = cv2.imread('../../redtest.jpg')
  
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
  
  
 #Red color rangle  169, 100, 100 , 189, 255, 255
  
+red_lower = np.array([110,50,50])
+red_upper = np.array([130,255,255])
+
+#if max BGR is in each position then return that colour
+
+mask = cv2.inRange(hsv, red_lower, red_upper)
+print(hsv) 
+
+#imshow requires a GUI and will error if you run the code headless 
+#cv2.imshow('image', img)
+#cv2.imshow('mask', mask)
  
-lower_range = np.array([110,50,50])
-upper_range = np.array([130,255,255])
  
-mask = cv2.inRange(hsv, lower_range, upper_range)
- 
-cv2.imshow('image', img)
-cv2.imshow('mask', mask)
- 
- 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
